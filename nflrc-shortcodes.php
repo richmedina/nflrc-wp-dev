90b19c40446cbaddbfa0b770add18a3771f48459
Richard Medina <rmedina@hawaii.edu>
2020-06-03 09:27:03 -1000

    contact page grid

--- nflrc-shortcodes.php
+++ nflrc-shortcodes.php
@@ -146,7 +146,7 @@ function nflrc_contact_grid_func($atts, $content = null) {
 	$args = array(
 		// 'numberposts' 		=> 1000,
 		// 'order'   			=> 'DESC',
-		'meta_query'     	=> array('key'=>'nflrc_staff','compare'=>'=','value'=>'t'),
+		'meta_query'     	=> array('key'=>'nflrc_staff','compare'=>'=','value'=>true),
 	    'post_type'      	=> 'contact',
 	    'posts_per_page' 	=> -1,
 
