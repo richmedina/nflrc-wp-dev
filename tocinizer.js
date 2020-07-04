6c440920fcc2f0ae1c07d575a4e612138c0feda7
Richard Medina <rmedina@hawaii.edu>
2020-07-04 10:16:26 -1000

    custom toc generator

---
+++ tocinizer.js
@@ -0,0 +1,32 @@
+MENU
+<div class="left"><ul id="toc" class="side" style="list-style-type: none;"></ul></div>
+<script>
+var sections = document.querySelectorAll('div.et_pb_post_content_0_tb_body > h2');
+var toc = document.querySelector('#toc');
+toc.append(sections.length);
+for (let i = 0; i < sections.length; i++) {
+    var s = sections[i];
+    var menuitem = document.createElement('li');
+    var anchor = document.createElement('a');
+    s.id = 'section' + i;
+    anchor.href = '#';
+    anchor.setAttribute('data-starget', '#'+s.id);
+    anchor.textContent = s.textContent;
+    anchor.classList.add('tocbtn');
+    menuitem.append(anchor);
+    toc.append(menuitem);
+
+    anchor.addEventListener('click', function(e) {
+    	e.preventDefault();
+
+    	section = document.querySelector(e.target.getAttribute('data-starget'));
+    	section.parentElement.style.height = '200%';
+    	section.scrollIntoView({behavior: 'smooth'});
+    	document.querySelectorAll('.active').forEach(function(element) {
+    		element.classList.remove('active');
+    	});
+    	section.classList.add('active');
+    	e.target.classList.add('active');
+    });        
+}
+</script>
