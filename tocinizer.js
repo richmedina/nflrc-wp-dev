ba5e5f5b6241fa8a67084ceff2d8eb31e1d34a68
Richard Medina <rmedina@hawaii.edu>
2020-07-04 10:22:45 -1000

    custom toc generator

--- tocinizer.js
+++ tocinizer.js
@@ -1,28 +1,31 @@
-var sections = document.querySelectorAll('div.et_pb_post_content_0_tb_body > h2');
-var toc = document.querySelector('#toc');
-toc.append(sections.length);
-for (let i = 0; i < sections.length; i++) {
-    var s = sections[i];
-    var menuitem = document.createElement('li');
-    var anchor = document.createElement('a');
-    s.id = 'section' + i;
-    anchor.href = '#';
-    anchor.setAttribute('data-starget', '#'+s.id);
-    anchor.textContent = s.textContent;
-    anchor.classList.add('tocbtn');
-    menuitem.append(anchor);
-    toc.append(menuitem);
+window.onload = function() {
+    var sections = document.querySelectorAll('div.et_pb_post_content_0_tb_body > h2');
+    var toc = document.querySelector('#toc');
+    for (let i = 0; i < sections.length; i++) {
+        var s = sections[i];
+        var menuitem = document.createElement('li');
+        var anchor = document.createElement('a');
+        s.id = 'section' + i;
+        anchor.href = '#';
+        anchor.setAttribute('data-starget', '#'+s.id);
+        anchor.textContent = s.textContent;
+        anchor.classList.add('tocbtn');
+        menuitem.append(anchor);
+        toc.append(menuitem);
 
-    anchor.addEventListener('click', function(e) {
-    	e.preventDefault();
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
 
-    	section = document.querySelector(e.target.getAttribute('data-starget'));
-    	section.parentElement.style.height = '200%';
-    	section.scrollIntoView({behavior: 'smooth'});
-    	document.querySelectorAll('.active').forEach(function(element) {
-    		element.classList.remove('active');
-    	});
-    	section.classList.add('active');
-    	e.target.classList.add('active');
-    });        
-}
+            section = document.querySelector(e.target.getAttribute('data-starget'));
+            section.parentElement.style.height = '200%';
+            section.scrollIntoView({behavior: 'smooth'});
+            document.querySelectorAll('.active').forEach(function(element) {
+                element.classList.remove('active');
+            });
+            section.classList.add('active');
+            e.target.classList.add('active');
+        });        
+    }
+};
+
+
