e82022a738f9fa3392ae91bcbd187aa671d73b83
Richard Medina <rmedina@hawaii.edu>
2020-07-04 14:36:16 -1000

    custom toc generator

--- tocinizer.js
+++ tocinizer.js
@@ -1,10 +1,18 @@
 window.onload = function() {
     var sections = document.querySelectorAll('.nflrc_divi_post_content > h2');
     var toc = document.querySelector('#toc');
+    var menuitem = document.createElement('li');
+    var anchor = document.createElement('a');
+    anchor.href = '#';
+    anchor.setAttribute('data-starget', '#');
+    anchor.textContent = 'Top';
+    anchor.classList.add('tocbtn');
+    menuitem.append(anchor);
+    toc.append(menuitem);    
     for (let i = 0; i < sections.length; i++) {
         var s = sections[i];
-        var menuitem = document.createElement('li');
-        var anchor = document.createElement('a');
+        menuitem = document.createElement('li');
+        anchor = document.createElement('a');
         s.id = 'section' + i;
         anchor.href = '#';
         anchor.setAttribute('data-starget', '#'+s.id);
